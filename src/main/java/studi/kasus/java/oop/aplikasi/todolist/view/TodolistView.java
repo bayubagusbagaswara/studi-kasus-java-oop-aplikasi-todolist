b7a7ea55a4a4cd4c6a31cd2f6bdb4771ebf8f9b6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-08 11:02:06 +0700

    membuat view untuk menampilkan layar aplikasi

---
+++ src/main/java/studi/kasus/java/oop/aplikasi/todolist/view/TodolistView.java
@@ -0,0 +1,15 @@
+package studi.kasus.java.oop.aplikasi.todolist.view;
+
+public class TodolistView {
+    public void showTodolist(){
+
+    }
+
+    public void addTodolist(){
+
+    }
+
+    public void removeTodolist(){
+
+    }
+}
