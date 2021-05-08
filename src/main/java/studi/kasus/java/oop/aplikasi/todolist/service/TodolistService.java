5d7636f68a0d432ec14c8895e8f522bc894c674a
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-05-08 10:58:47 +0700

    membuat service yang mengimplementasikan method sesuai business logic

---
+++ src/main/java/studi/kasus/java/oop/aplikasi/todolist/service/TodolistService.java
@@ -0,0 +1,12 @@
+package studi.kasus.java.oop.aplikasi.todolist.service;
+
+public interface TodolistService {
+
+    // implementasi method menurut business logic
+
+    void showTodolist();
+
+    void addTodolist(String todo);
+
+    void removeTodolist(Integer number);
+}
